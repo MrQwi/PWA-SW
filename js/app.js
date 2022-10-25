99df0b9c1e4e94eb5d036fe3e65ee4192787e3b0
MrQwi <jorshglum@gmail.com>
2022-10-25 13:13:20 +0300

    Prikolv1

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', async () => {
+    if ('serviceWorker' in navigator) {
+      try {
+        const reg = await navigator.serviceWorker.register('/sw.js')
+        console.log('Service worker register success', reg)
+      } catch (e) {
+        console.log('Service worker register fail')
+      }
+    }
+ })
+ if ('serviceWorker' in navigator){
+    function lol(){ isid.innerHTML='[SW]: register success'; }
+    
+}
+function lok(){ isid.innerHTML=' '; }
+
+
